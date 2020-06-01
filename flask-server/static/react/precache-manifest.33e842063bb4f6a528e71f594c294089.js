6735066864107da1328c05815bcd65ae1b241cdd
Raymond Lee <raymond@Raymonds-MacBook-Pro-2.local>
2020-05-31 23:29:50 -0400

    built

---
+++ flask-server/static/react/precache-manifest.33e842063bb4f6a528e71f594c294089.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9dac02e92f7aafd3508dc7322802161",
+    "url": "/static/react/../../flask-server/templates/index.html"
+  },
+  {
+    "revision": "c1fb97dbbd4947fb427e",
+    "url": "/static/react/css/main.4eb546f7.chunk.css"
+  },
+  {
+    "revision": "335e24b8b5bc480bd23e",
+    "url": "/static/react/js/2.92b8907a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.92b8907a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1fb97dbbd4947fb427e",
+    "url": "/static/react/js/main.9828053d.chunk.js"
+  },
+  {
+    "revision": "a6017aa3b494f0679037",
+    "url": "/static/react/js/runtime-main.2314581f.js"
+  },
+  {
+    "revision": "8a1c49623a45e1a1a4f828b38d39c402",
+    "url": "/static/react/media/consciouscad_logo.8a1c4962.png"
+  },
+  {
+    "revision": "6aca59716751c074cc6acaa844674e9d",
+    "url": "/static/react/media/main.6aca5971.png"
+  }
+]);
